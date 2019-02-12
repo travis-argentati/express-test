47574195a86944a30b4ba42b18522d0ef96dddd9
travis-argentati <argentatit@xavier.edu>
2019-02-12 17:04:25 -0500

    Update books-api.js

--- server/books-api.js
+++ server/books-api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const authors = require('./authors');
+const authors = require('./books');
 
 router.get('/', (req, res) => authors.getAll(req, res));
 router.get('/:id', (req, res) => authors.getById(req, res));
